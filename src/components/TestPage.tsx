4f28e26bca08f0a72cb2fce4a74f67518328bd93
Xinwei Fan <xinwei.fan19@gmail.com>
2025-05-17 06:40:57 -0700

    added a test page for test the page quickly

---
+++ src/components/TestPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const TestPage: React.FC = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl font-bold">Mac mini server is working!</h1>
+    </div>
+  );
+}; 
